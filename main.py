4ce25ea07e499e5ddd119e5ad41821d97bf486b1
hiirigma <hariton1599@gmail.com>
2020-12-22 00:48:14 +0300

    All functions added

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ class ExampleApp(QtWidgets.QMainWindow, PaGeMain.Ui_Dialog):
         # Это здесь нужно для доступа к переменным, методам
         # и т.д. в файле design.py
         super().__init__()
+
+        adapt_list = get_if_list()
+        for adapt in adapt_list:
+            self.AdapterComboBox.addItem(adapt)
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
         self.cancelButton.clicked.connect(self.ex_prog)
         self.sendButton.clicked.connect(self.send_packet)
@@ -60,7 +64,7 @@ class ExampleApp(QtWidgets.QMainWindow, PaGeMain.Ui_Dialog):
         dIPIhl = int(self.IHLEdit.toPlainText())
         dIPID = int(self.IDEdit.toPlainText())
         dTTL = int(self.TTLEdit.toPlainText())
-        dProto = int(self.ProtocolEdit.toPlainText())
+        sProto = self.ProtocolEdit.toPlainText()
         if (self.ChsumCheck.isEnabled()):
             dChkSum = random.randint()
         else:
@@ -105,21 +109,136 @@ class ExampleApp(QtWidgets.QMainWindow, PaGeMain.Ui_Dialog):
             sDstMAC = genRandMAC()
         else:
             sDstMAC = self.MacDstEdit.toPlainText()
-        
 
+        packet = IP(dst=sIPSrc, src=sIPDst, version = dIPVer, ihl = dIHl, tos = dTOS, len = dTL, id = dIPID, flags = dIPFlag, frag = dFrag, ttl = dTTL, proto = sProto, chksum = dChkSum)
+
+        dPortSrc = 0
+        dPortDst = 0
+        dSeqNum = 0
+        dAckNum = 0
+        dWinNum = 0
+        dTCPChsum = 0
+        dUDPChsum = 0
+        dUDPLen = 0
+        dICMPType = 0
+        dICMPCode = 0
+        dICMPID = 0
+        dICMPSeq = 0
+        dICMPMsg = 0
 # TCP
         if (dCurIdx == 0):
+            if (self.PortSrcCheck.toPlainText())
+                dPortSrc = random.randint()
+            else:
+                dPortSrc = int(self.PortSrcEdit.toPlainText())
 
+            if (self.PortDestCheck.toPlainText())
+                dPortDst = random.randint()
+            else:
+                dPortDst = int(self.PortDestEdit.toPlainText())
+
+            if (self.SeqNCheck.toPlainText())
+                dSeqNum = random.randint()
+            else:
+                dSeqNum = int(self.SeqNEdit.toPlainText())
+
+            if (self.AckNCheck.toPlainText())
+                dAckNum = random.randint()
+            else:
+                dAckNum = int(self.AckNEdit.toPlainText())                
+
+            if (self.WinCheck.toPlainText())
+                dWinNum = random.randint()
+            else:
+                dWinNum = int(self.WinEdit.toPlainText())  
+
+            if (self.UrgCheck.toPlainText())
+                dUrgNum = random.randint()
+            else:
+                dUrgNum = int(self.UrgEdit.toPlainText())  
+
+            dOff = int(self.OffsEdit.toPlainText())  
+
+            if (self.TCPChsumCheck.toPlainText())
+                dTCPChsum = random.randint()
+            else:
+                dTCPChsum = int(self.TCPChsumEdit.toPlainText())  
+
+            if (self.TCPChsumCheck.toPlainText())
+                dTCPChsum = random.randint()
+            else:
+                dTCPChsum = int(self.TCPChsumEdit.toPlainText())  
+
+            dTCPFlags = 0x000000000
+            if (self.URGCheck.toPlainText())
+                dTCPFlags |= 0x000000001          
+
+            if (self.ACKCheck.toPlainText())
+                dTCPFlags |= 0x000000010 
+
+            if (self.PSHCheck.toPlainText())
+                dTCPFlags |= 0x000000100 
+
+            if (self.RSTCheck.toPlainText())
+                dTCPFlags |= 0x000001000 
+
+            if (self.SYNCheck.toPlainText())
+                dTCPFlags |= 0x000010000 
+
+            if (self.FINCheck.toPlainText())
+                dTCPFlags |= 0x000100000 
+
+            if (self.CWRCheck.toPlainText())
+                dTCPFlags |= 0x001000000
+
+            if (self.ECECheck.toPlainText())
+                dTCPFlags |= 0x010000000
+
+            packet /= TCP(sport = dPortSrc, dport = dPortDst, seq = dSeqNum, ack = dAckNum, dataofs = dOff, flags = dTCPFlags, windows = dWinNum, chksum = dTCPChsum, urgptr = dUrgNum)
 
 # UDP
         if (dCurIdx == 1):
 
+            if (self.UDPPortDestCheck.toPlainText())
+                dPortSrc = random.randint()
+            else:
+                dPortSrc = int(self.UDPPortSrcEdit.toPlainText())
+
+            if (self.UDPPortDestCheck.toPlainText())
+                dPortDst = random.randint()
+            else:
+                dPortDst = int(self.UDPPortDestEdit.toPlainText())
+
+            if (self.UDPChsumCheck.toPlainText())
+                dUDPChsum = random.randint()
+            else:
+                dUDPChsum = int(self.UDPChsumEdit.toPlainText())  
+
+            if (self.UDPLenCheck.toPlainText())
+                dUDPLen = random.randint()
+            else:
+                dUDPLen = int(self.UDPLenEdit.toPlainText())  
+
+            packet /= UDP(sport = dPortSrc, dport = dPortDst, chksum = dUDPChsum, len = dUDPLen)
 
 # ICMP   
         if (dCurIdx == 2):
+            dICMPCode = int(self.CodeICMPEdit.toPlainText())  
+            dICMPID = int(self.IDICMPEdit.toPlainText())  
+            dICMPSeq = int(self.SeqICMPEdit.toPlainText())  
+            dIdx = self.ICMPMCombo.currentIndex()
+            if (dIdx == 0):
+                dICMPMsg = 0
+            else:
+                dICMPMsg = 8
+            packet /= ICMP(type = dICMPMsg, code = dICMPCode, id = dICMPID, seq = dICMPSeq)
 
 
+        sData = self.FinalDataEdit.toPlainText()
+        packet /= sData
 
+        sAdapter = self.AdapterComboBox.currentText()
+        sendp(packet,sAdapter)
         print ('Packet send')    
 
 def main():
